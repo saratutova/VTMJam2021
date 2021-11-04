de6e2b35b8b9debb2a8cc913624a1d36ea1e6882
saratutova <saramtutova@gmail.com>
2021-11-04 20:11:23 +0100

    updated the main menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -8,12 +8,30 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private Button _startButton;
+    [SerializeField] private Button _creditsButton;
+    [SerializeField] private Button _exitButton;
 
     [SerializeField] private string sceneName;
 
     private void Start()
     {
         _startButton.onClick.AddListener(StartClicked);
+        _creditsButton.onClick.AddListener(CreditsClicked);
+        _exitButton.onClick.AddListener(ExitClicked);
+    }
+
+    private void ExitClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void CreditsClicked()
+    {
+        //TODO: Turn on the credits
     }
 
     private void StartClicked()
