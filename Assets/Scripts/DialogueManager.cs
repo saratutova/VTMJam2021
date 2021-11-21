fb8b2799f7eaa7838d87df92238f9c1e35e1e878
saratutova <sarajujeczka@gmail.com>
2021-11-21 15:37:11 +0100

    fixes

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,16 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using Yarn.Unity;
 
 public class DialogueManager : Manager<DialogueManager>
 {
+    [SerializeField] private GameObject _backgroundsGO = default;
     [SerializeField] private Image _background = default;
     [SerializeField] private Image _character = default;
-    [SerializeField] DialogueRunner _dialogueRunner = default;
-    [SerializeField] List<DialogueData> _programs = new List<DialogueData>();
+    [SerializeField] private DialogueRunner _dialogueRunner = default;
+    [SerializeField] private List<DialogueData> _programs = new List<DialogueData>();
+    [HideInInspector] public UnityEvent DialogueCompleted = new UnityEvent();
+
+    public bool IsDuringDialogue => _isRunning;
+
+    private bool _isRunning;
+
+    private void Start()
+    {
+        _dialogueRunner.onDialogueComplete.AddListener(OnDialogueComplite);
+    }
+
+    private void OnDialogueComplite()
+    {
+        _isRunning = false;
+        _backgroundsGO.SetActive(false);
+        DialogueCompleted.Invoke();
+        DialogueCompleted.RemoveAllListeners();
+    }
 
     public void StartDialogue(string name)
     {
@@ -20,6 +41,8 @@ public class DialogueManager : Manager<DialogueManager>
             Debug.LogError($"Couldn't find program named: {name}");
             return;
         }
+        _isRunning = true;
+        _backgroundsGO.SetActive(true);
         _background.gameObject.SetActive(program.background != null);
         _background.sprite = program.background;
         _character.gameObject.SetActive(program.character != null);
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : Manager<GameManager>
 {
     [SerializeField] private Scenery _startScenery;
     [SerializeField] private float _fadeTime = 5f;
+    [HideInInspector] public UnityEvent FocusChanged = new UnityEvent();
+
+    private int _focus = 0;
+
+    public int Focus => _focus;
 
     protected override void Awake()
     {
@@ -25,15 +31,24 @@ public class GameManager : Manager<GameManager>
                 { 
                     MessageManager.Instance.TurnOff();
                     DialogueManager.Instance.StartDialogue("Slajd4");
+                    DialogueManager.Instance.DialogueCompleted.AddListener(() => FadeManager.Instance.FadeBreak(1f));
                 }); 
                 //FadeManager.Instance.FadeEnded.AddListener(FadedFinished); 
             }
         );
     }
 
-    //private void FadedFinished()
-    //{
-    //    //After Fade OUT - start dialogue
-        
-    //}
+    public void ChangeFocus(int amount)
+    {
+        _focus += amount;
+        if (_focus < 0)
+        {
+            _focus = 0;
+        }
+        if (_focus > 5)
+        {
+            _focus = 5;
+        }
+        FocusChanged.Invoke();
+    }
 }
