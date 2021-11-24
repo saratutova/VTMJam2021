e4c6e7addc8e416a3e80a8d9fcb955c92de1a0d5
saratutova <sarajujeczka@gmail.com>
2021-11-24 22:51:15 +0100

    added the help button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,11 +7,12 @@ public class GameManager : Manager<GameManager>
 {
     [SerializeField] private Scenery _startScenery;
     [SerializeField] private float _fadeTime = 5f;
+    [SerializeField] private GameManagerModel _model = default;
     [HideInInspector] public UnityEvent FocusChanged = new UnityEvent();
 
-    private int _focus = 0;
-
-    public int Focus => _focus;
+    public int Focus => _model.Focus;
+    public List<Item> Items => _model.Items;
+    public List<Memory> Memories => _model.Memories;
 
     private void Start()
     {
@@ -33,15 +35,35 @@ public class GameManager : Manager<GameManager>
 
     public void ChangeFocus(int amount)
     {
-        _focus += amount;
-        if (_focus < 0)
+        _model.Focus += amount;
+        if (Focus < 0)
         {
-            _focus = 0;
+            _model.Focus = 0;
         }
-        if (_focus > 5)
+        if (Focus > 5)
         {
-            _focus = 5;
+            _model.Focus = 5;
         }
         FocusChanged.Invoke();
+    }
+
+    public void AddItem(Item item)
+    {
+        _model.AddItem(item);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        _model.RemoveItem(item);
+    }
+
+    public void AddMemory(Memory memory)
+    {
+        _model.AddMemory(memory);
+    }
+
+    public void RemoveMemory(Memory memory)
+    {
+        _model.RemoveMemory(memory);
     }
 }
